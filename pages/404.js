93a6cf401f9d3405ffb324529fee46e5f68c4a7f
mohamed <mohamed.mohamed.yossef@gmail.com>
2021-05-20 14:47:16 +0200

    add Head to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { useEffect } from "react"
 import { useRouter } from "next/router"
+import Head from "next/head"
 
 const NotFound = () => {
   const router = useRouter()
@@ -10,24 +11,30 @@ const NotFound = () => {
     }, 5500)
   }, [])
   return (
-    <div className="not-found">
-      <h1>404</h1>
-      <h2>Ooops! Page not found :(</h2>
-      <p>
-        Back to <Link href="/">Home page</Link>
-      </p>
-      <style jsx>{`
-        .not-found {
-          background: #fff;
-          padding: 30px;
-          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
-          transform: rotateZ(-1deg);
-        }
-        h1 {
-          font-size: 2.5rem;
-        }
-      `}</style>
-    </div>
+    <>
+      <Head>
+        <title>404 | Error</title>
+        <meta name="description" content="Eat and spread the joy" />
+      </Head>
+      <div className="not-found">
+        <h1>404</h1>
+        <h2>Ooops! Page not found :(</h2>
+        <p>
+          Back to <Link href="/">Home page</Link>
+        </p>
+        <style jsx>{`
+          .not-found {
+            background: #fff;
+            padding: 30px;
+            box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+            transform: rotateZ(-1deg);
+          }
+          h1 {
+            font-size: 2.5rem;
+          }
+        `}</style>
+      </div>
+    </>
   )
 }
 
